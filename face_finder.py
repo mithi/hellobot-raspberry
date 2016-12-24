9e682099645a8be97460e87054443b0c5672c682
Mithi <mithi.sevilla@gmail.com>
2016-12-24 23:58:14 +0000

    face finder slow on rpi2

--- face_finder.py
+++ face_finder.py
@@ -2,52 +2,48 @@ from picamera import PiCamera
 import picamera.array
 import cv2 
 import numpy 
-import io 
+import os
 
 CASCADE_PATH = '/home/pi/hellobot/cascade/haarcascade_frontalface_default.xml'
+SHOW_IMAGE = "sudo fbi -T 1 -d /dev/fb0 -a -noverbose /home/pi/hellobot/images/current2.jpg &"
+CURRENT_IMAGE_PATH = "/home/pi/hellobot/images/current.jpg"
+CURRENT_IMAGE_PATH_WITH_RECTANGLE = "/home/pi/hellobot/images/current2.jpg"
+
 
 class FaceFinder:
 
   def __init__(self):
-    self.stream = io.BytesIO()
     self.camera = PiCamera()
     self.camera.resolution = (320, 240)
-    self.camera.framerate = 24
     self.cascade = cv2.CascadeClassifier(CASCADE_PATH) 
-    self.buff = None
-    self.image = None
-    self.gray = None
-    self.faces = None
 
   def run(self):
 
     #get picture
-    self.camera.capture(self.stream, format='jpeg')
-    
-    #convert picture to array 
-    self.buff = numpy.fromstring(self.stream.getvalue(), dtype=numpy.uint8)
-    
+    self.camera.capture(CURRENT_IMAGE_PATH, format='jpeg')
+
     #convert this to opencv image
-    self.image = cv2.imdecode(self.buff, 1)
+    image = cv2.imread(CURRENT_IMAGE_PATH, 1)
 
     #convert this to grey
-    self.gray = cv2.cvtColor(self.image, cv2)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     #look for face
-    self.faces = face_cascade.detectMultiScale(self.gray, 1.1, 5)
+    faces = self.cascade.detectMultiScale(gray, 1.1, 5)
 
-    print "found: " + str(len(self.faces)) + " face(s)"
+    if len(faces) > 0:
+
+      print "found: " + str(len(faces)) + " face(s)"
     
-    #put rectable on face
-    for (x, y, w, h) in self.faces:
-      cv2.rectangle(self.image,(x, y),(x + w, y + h),(255,255,0),2)
-    
-    #show picture
-    cv2.imshow(self.image)
-
-  def save(self):
-
-    cv2.imwrite("current.jpg", self.image)
-
+      #put rectable on face
+      for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y),(x + w, y + h),(255,255,0), 2)
+     
+      #show picture
+      cv2.imwrite(CURRENT_IMAGE_PATH_WITH_RECTANGLE, image)
+      os.system(SHOW_IMAGE)
+      
+    else:
+      print "no face detected"
 
 
