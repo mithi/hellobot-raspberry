bc5fe9859fdbfc2f8683e58db25a15deae9c1b95
Mithi <mithi.sevilla@gmail.com>
2016-12-28 21:05:23 +0800

    clean up for readability

--- face_finder.py
+++ face_finder.py
@@ -6,7 +6,7 @@ import os
 from time import sleep
 
 CASCADE_PATH = '/home/pi/hellobot/cascade/haarcascade_frontalface_default.xml'
-SHOW_IMAGE2 = "sudo fbi -T 1 -d /dev/fb0 -a -noverbose /home/pi/hellobot/images/current2.jpg &"
+SHOW_FACE_IMAGE = "sudo fbi -T 1 -d /dev/fb0 -a -noverbose /home/pi/hellobot/images/current2.jpg &"
 CURRENT_IMAGE_PATH = "/home/pi/hellobot/images/current.jpg"
 CURRENT_MARKED_IMAGE_PATH = "/home/pi/hellobot/images/current2.jpg"
 SHOW_IMAGE = "sudo fbi -T 1 -d /dev/fb0 -a -noverbose /home/pi/hellobot/images/current.jpg &"
@@ -32,7 +32,7 @@ class FaceFinder:
     self.camera.capture(CURRENT_IMAGE_PATH)                     #get picture
     self.image = cv2.imread(CURRENT_IMAGE_PATH, 1)              #convert this to opencv image
     gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)         #convert this to grey    
-    self.faces = self.cascade.detectMultiScale(image = gray, scaleFactor = 1.4, minNeighbors = 6, minSize = (30, 30), maxSize = (240, 240))    #look for face -- FIXME: tweak middle variable for performance
+    self.faces = self.cascade.detectMultiScale(image = gray, scaleFactor = 1.4, minNeighbors = 6, minSize = (30, 30), maxSize = (240, 240))
     # see http://stackoverflow.com/questions/20801015/recommended-values-for-opencv-detectmultiscale-parameters
 
     if len(self.faces):
@@ -45,23 +45,24 @@ class FaceFinder:
 
     if update: self.run()
 
-    if self.has_faces and self.has_faces_previous: #if shows face before, stop showing previous boxed image, show current boxed face
-      os.system(SHOW_IMAGE2)
-    elif self.has_faces: # if first time with face, stop camera preview, then show boxed face
+    if self.has_faces and self.has_faces_previous:
+      os.system(REMOVE_IMAGE)
+      os.system(SHOW_FACE_IMAGE)
+    elif self.has_faces:
       self.camera.stop_preview() 
-      os.system(SHOW_IMAGE2) 
-    elif self.has_faces_previous: #if shows face but now there's no more face, stop showing previous boxed face, show camera preview
+      os.system(SHOW_FACE_IMAGE) 
+    elif self.has_faces_previous: 
       os.system(REMOVE_IMAGE)
       self.camera.start_preview()
  
   def box_faces(self):
-    
-    for (x, y, w, h) in self.faces:
-      cv2.rectangle(self.image, (x, y),(x + w, y + h), (255, 255, 0), 2) #put rectangle on each face
-     
-    cv2.imwrite(CURRENT_MARKED_IMAGE_PATH, self.image) #save picture
     #print "found: " + str(len(self.faces)) + " face(s)"
-
+    #put rectangle on each face and save picture    
+    for (x, y, w, h) in self.faces:
+      cv2.rectangle(self.image, (x, y),(x + w, y + h), (255, 255, 0), 2)
+     
+    cv2.imwrite(CURRENT_MARKED_IMAGE_PATH, self.image)
+    
   def face_detected(self, update = True):
 
     if update: self.run()
@@ -70,4 +71,5 @@ class FaceFinder:
   def shutdown(self):
     if self.has_faces:
       os.system(REMOVE_IMAGE)
-    self.camera.stop_preview()
+    else:
+      self.camera.stop_preview()
