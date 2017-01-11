f26568fff4cff4a0106e6867405a3cb3a7f32775
Mithi <mithi.sevilla@gmail.com>
2017-01-11 14:57:57 +0800

    Update robot_script.py

--- robot_script.py
+++ robot_script.py
@@ -1,5 +1,5 @@
 from gpiozero import MotionSensor
-from robot_modules import Listener, Responder, Directive, get_response, camera_stop
+from robot_modules import Listener, Responder, Directive, get_response
 from face_finder import FaceFinder
 from relayer import Relayer
 from time import sleep 
@@ -9,24 +9,14 @@ COUNTS = 10000
 TRIGGER_WORD = "robot"
 PIR_PIN = 26
 
-#camera_stop()               #make sure to stop resources using camera
-
-listener = Listener()       # listens to microphone and outputs text
-responder = Responder()     # plays video on screen 
-relayer = Relayer()         # communicates to arduino
+listener = Listener()                  # listens to microphone and outputs text
+responder = Responder()                # plays video on screen 
+relayer = Relayer()                    # communicates to arduino
 directive = Directive(TRIGGER_WORD)
 pir = MotionSensor(PIR_PIN)
 face_finder = FaceFinder()
 
-def move(key):
-  relayer.command(key) 
-
-def obey(key):
-  if key in ['forward', 'backward', 'left', 'right']: move(key)
-  if key == 'death': responder.shutdown()
-  if key == 'camera': robot_camera()
-
-def robot_camera():
+def smart_camera():
 
   COUNT = 1000
 
@@ -34,23 +24,27 @@ def robot_camera():
     face_finder.show()
   
   face_finder.shutdown()
+  
+def shutdown():
+  SHUTDOWN = "sudo shutdown -h now"
+  os.system(SHUTDOWN)
+  
+def obey(key):
+  if key in ['forward', 'backward', 'left', 'right']: relayer.command(key)
+  if key == 'death': shutdown()
+  if key == 'camera': smart_camera()
 
 def listen():
-
   relayer.signal("listening")
   phrase = listener.hear()
   relayer.signal("message decoded")
   return phrase
 
 def reply(phrase):
-  
   relayer.command("move arms")
-  response = get_response(phrase) # picks one of several appropriate responses (video title) 
-                                  # based on whether the given phrase contains a specific keyword 
-  responder.show(response)        # plays the corresponding video given the video title 
+  responder.show(get_response(phrase))        # plays the corresponding video given the video title 
 
 def interact():
-
   phrase = listen()
   if phrase:
     key = directive.command(phrase)
@@ -66,7 +60,6 @@ def greet():
 
 responder.default()               #shows default eye image on screen
 relayer.connect()                 #connect to arduino port
-face_finder.shutdown()
 
 while True:
   
@@ -82,5 +75,4 @@ while True:
   responder.sleep()
   print "no person detected"
 
-camera_stop()
 
